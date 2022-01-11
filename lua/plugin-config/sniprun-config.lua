aaa868dd1b0a2f3c54207d834ed6e4017192f96d
Vidocq <vidocqho@gmail.com>
2022-01-11 18:22:15 +0800

    edited sniprun configs

--- lua/plugin-config/sniprun-config.lua
+++ lua/plugin-config/sniprun-config.lua
@@ -3,10 +3,21 @@ require'sniprun'.setup({
   --# you can combo different display modes as desired
   --
   display = {
+    "VirtualTextOk",    --# display ok results as virtual text (multiline is shortened)
+    -- "LongTempFloatingWindow",  --# same as above, but only long results. To use with VirtualText__
     "NvimNotify",              --# display with the nvim-notify plugin
   },
 
   display_options = {
+    terminal_width = 45,
     notification_timeout = 15   --# timeout for nvim_notify output
   },
+
+    --# You can use the same keys to customize whether a sniprun producing
+  --# no output should display nothing or '(no output)'
+  show_no_output = {
+    "Classic",
+    "TempFloatingWindow",      --# implies LongTempFloatingWindow, which has no effect on its own
+  },
+
 })
