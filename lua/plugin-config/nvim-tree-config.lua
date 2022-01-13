d2e974704432dfc5e92e2933be8f23fd2e38582a
Vidocq <vidocqho@gmail.com>
2022-01-14 02:34:39 +0800

    auto resize nvim tree after open a file

--- lua/plugin-config/nvim-tree-config.lua
+++ lua/plugin-config/nvim-tree-config.lua
@@ -45,7 +45,7 @@ require'nvim-tree'.setup {
     height = 30,
     hide_root_folder = false,
     side = 'right',
-    auto_resize = false,
+    auto_resize = true,
     mappings = {
       custom_only = false,
       list = {}
