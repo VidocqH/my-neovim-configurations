af3d1ead6703c7dcbecebfb66ce7bf991299a9cd
Vidocq <vidocqho@gmail.com>
2022-06-21 14:25:19 +0800

    update drpc settings

--- lua/plugin-config/discord-presence.lua
+++ lua/plugin-config/discord-presence.lua
@@ -2,7 +2,7 @@
 require("presence"):setup({
     -- General options
     neovim_image_text   = "\"Vim, or Neovim, that is the question.\" By William Shakespeare", -- Text displayed when hovered over the Neovim image
-    main_image          = "file",                   -- Main image display (either "neovim" or "file")
+    main_image          = "neovim",                   -- Main image display (either "neovim" or "file")
     enable_line_number  = false,                      -- Displays the current line number instead of the current project
     blacklist           = {},                         -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
     buttons             = true,                       -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
