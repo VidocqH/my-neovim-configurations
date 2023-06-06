cb369e1db3bc3b88edf821fcd8433aa4a1cd8e9e
Vidocq <vidocqho@gmail.com>
2023-06-06 15:30:00 +0800

    fix statuscol

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,6 +1,6 @@
 -- vim.g.neovide_fullscreen = true
 vim.g.neovide_remember_window_size = true
 vim.g.neovide_cursor_vfx_mode = "railgun"
-vim.opt.guifont = { "LigaRobotoMono Nerd Font", ":h16" }
+vim.opt.guifont = { "LigaRobotoMono Nerd Font", ":h14" }
 vim.g.neovide_input_macos_alt_is_meta = true
 
--- lua/plugin-config/statuscol-config.lua
+++ lua/plugin-config/statuscol-config.lua
@@ -2,8 +2,9 @@ local builtin = require("statuscol.builtin")
 require("statuscol").setup({
   relculright = true,
   segments = {
-    { text = { "%s" }, click = "v:lua.ScSa", sign={ maxwidth = 1, colwidth = 1 } },
-    { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-    { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+    { click = "v:lua.ScSa", sign = { name = { ".*" } } },
+    { click = "v:lua.ScSa", sign = { name = { "GitSigns" }, colwidth = 1, maxwidth = 1 } },
+    { click = "v:lua.ScFa", text = { builtin.foldfunc } },
+    { click = "v:lua.ScLa", text = { builtin.lnumfunc, " " } },
   },
 })
