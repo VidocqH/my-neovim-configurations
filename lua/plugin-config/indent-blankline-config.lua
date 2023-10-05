c5c05416d7b07e7cfccb6c8d6a0ca5dc40b2fbe0
Vidocq <vidocqho@gmail.com>
2023-10-05 16:21:56 +0800

    indent blankline update to v3

--- lua/plugin-config/indent-blankline-config.lua
+++ lua/plugin-config/indent-blankline-config.lua
@@ -2,16 +2,18 @@ vim.opt.list = true
 
 -- vim.cmd [[highlight IndentBlankLineContextChar guifg=#e5c07b]]
 
-require("indent_blankline").setup {
-  use_treesitter = true,
-  space_char_blankline = " ",
-  show_current_context = false,
-  show_current_context_start = false,
-  buftype_exclude = { "terminal" },
-  filetype_exclude = { "dashboard", "mason", "lazy", "neo-tree", "lspsagaoutline", "aerial" },
-}
-
-require('mini.indentscope').setup({
-  symbol = '│',
+require("ibl").setup({
+  indent = { char = "▎" },
 })
+-- require("indent_blankline").setup {
+-- use_treesitter = true,
+-- space_char_blankline = " ",
+-- show_current_context = false,
+-- show_current_context_start = false,
+-- buftype_exclude = { "terminal" },
+-- filetype_exclude = { "dashboard", "mason", "lazy", "neo-tree", "lspsagaoutline", "aerial" },
+-- }
 
+require("mini.indentscope").setup({
+  symbol = "│",
+})
