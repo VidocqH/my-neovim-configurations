b2b4afd86c3b212ab1364d86a67abee47796f460
Vidocq <vidocqho@gmail.com>
2023-10-09 16:07:06 +0800

    fix git signs statuscol

--- lua/plugin-config/gitsigns-config.lua
+++ lua/plugin-config/gitsigns-config.lua
@@ -1,23 +1,23 @@
-require('gitsigns').setup {
-  signs = {
-    add          = { text = '█' },
-    change       = { text = '█' },
-    delete       = { text = '█' },
-    topdelete    = { text = '█' },
-    changedelete = { text = '█' },
-    untracked    = { text = '█' },
-  },
--- '█' 
-  signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
-  current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
-  current_line_blame_opts = {
-    virt_text = true,
-    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-    delay = 100,
-    ignore_whitespace = false,
-  },
-  current_line_blame_formatter_opts = {
-    relative_time = true
-  },
-  sign_priority = 0,
-}
+require("gitsigns").setup({
+	signs = {
+		add = { text = "█" },
+		change = { text = "█" },
+		delete = { text = "█" },
+		topdelete = { text = "█" },
+		changedelete = { text = "█" },
+		untracked = { text = "█" },
+	},
+	-- '█'
+	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+	current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+	current_line_blame_opts = {
+		virt_text = true,
+		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+		delay = 100,
+		ignore_whitespace = false,
+	},
+	current_line_blame_formatter_opts = {
+		relative_time = true,
+	},
+	sign_priority = 0,
+})
--- lua/plugin-config/statuscol-config.lua
+++ lua/plugin-config/statuscol-config.lua
@@ -3,7 +3,8 @@ require("statuscol").setup({
   relculright = true,
   segments = {
     { click = "v:lua.ScSa", sign = { name = { ".*" } } },
-    { click = "v:lua.ScSa", sign = { name = { "GitSigns" }, colwidth = 1, maxwidth = 1 } },
+    { click = "v:lua.ScSa", sign = { name = { "dap*", "neotest*" }, maxwidth = 1, auto = true } },
+    { click = "v:lua.ScSa", sign = { namespace = { "gitsigns*" }, colwidth = 1, maxwidth = 1, auto = false } },
     { click = "v:lua.ScFa", text = { builtin.foldfunc } },
     { click = "v:lua.ScLa", text = { builtin.lnumfunc, " " } },
   },
