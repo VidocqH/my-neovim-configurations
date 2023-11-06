24d44daf8cac371b1f2a3bfa4b436669ef459c2f
Vidocq <vidocqho@gmail.com>
2023-11-06 17:21:07 +0800

    upgrade ts setting

--- lua/plugin-config/nvim-treesitter-config.lua
+++ lua/plugin-config/nvim-treesitter-config.lua
@@ -1,5 +1,5 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"vim", "lua", "javascript", "json", "typescript", "python", "cpp", "c"},
+require("nvim-treesitter.configs").setup({
+  ensure_installed = { "vim", "lua", "javascript", "json", "typescript", "python", "cpp", "c" },
 
   -- Install languages synchronously (only applied to `ensure_installed`)
   sync_install = true,
@@ -16,16 +16,16 @@ require'nvim-treesitter.configs'.setup {
     enable = true,
     keymaps = {},
     -- keymaps = {
-      -- init_selection = '<CR>',
-      -- node_incremental = '<CR>',
-      -- node_decremental = '<BS>',
-      -- scope_incremental = '<TAB>',
+    -- init_selection = '<CR>',
+    -- node_incremental = '<CR>',
+    -- node_decremental = '<BS>',
+    -- scope_incremental = '<TAB>',
     -- }
   },
 
   -- Code Formatted Module
   indent = {
-    enable = true
+    enable = true,
   },
 
   -- Rainbow parentheses
@@ -38,35 +38,36 @@ require'nvim-treesitter.configs'.setup {
     enable = true,
     enable_autocmd = false,
   },
-}
+})
 
 -- Foldding Module
-vim.wo.foldmethod = 'expr'
-vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
 
 -- Treesitter context config
-require'treesitter-context'.setup{
+require("treesitter-context").setup({
   zindex = 2000, -- The Z-index of the context window
-  mode = 'topline',  -- Line used to calculate context. Choices: 'cursor', 'topline'
-}
+  mode = "topline", -- Line used to calculate context. Choices: 'cursor', 'topline'
+  multiline_threshold = 10,
+})
 
-require 'rainbow-delimiters.setup' {
+require("rainbow-delimiters.setup")({
   query = {
-    [''] = 'rainbow-delimiters',
-    latex = 'rainbow-blocks',
-    html = 'rainbow-delimiters',
-    jsx = 'rainbow-delimiters-react',
-    tsx = 'rainbow-delimiters-react',
-    vue = 'rainbow-delimiters'
+    [""] = "rainbow-delimiters",
+    latex = "rainbow-blocks",
+    html = "rainbow-delimiters",
+    jsx = "rainbow-delimiters-react",
+    tsx = "rainbow-delimiters-react",
+    vue = "rainbow-delimiters",
   },
   highlight = {
-    'RainbowDelimiterYellow',
-    'RainbowDelimiterBlue',
-    'RainbowDelimiterOrange',
-    'RainbowDelimiterGreen',
-    'RainbowDelimiterViolet',
-    'RainbowDelimiterCyan',
-    'RainbowDelimiterRed',
+    "RainbowDelimiterYellow",
+    "RainbowDelimiterBlue",
+    "RainbowDelimiterOrange",
+    "RainbowDelimiterGreen",
+    "RainbowDelimiterViolet",
+    "RainbowDelimiterCyan",
+    "RainbowDelimiterRed",
   },
   -- blacklist = {'c', 'cpp'},
-}
+})
