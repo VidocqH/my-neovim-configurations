e1434f30c7dfb99329eab2c796d0227b7685680f
Vidocq <vidocqho@gmail.com>
2024-01-01 13:21:58 +0800

    fix icon error

--- lua/lsp/setup.lua
+++ lua/lsp/setup.lua
@@ -2,48 +2,50 @@
 
 local function lsp_settings(server_name)
   if server_name == "lua_ls" then
-    return require "lsp.lsp_settings.lua_ls"
+    return require("lsp.lsp_settings.lua_ls")
   elseif server_name == "tsserver" then
-    return require "lsp.lsp_settings.tsserver"
+    return require("lsp.lsp_settings.tsserver")
   end
   return {}
 end
 
 -- Diagnostics Sign
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = " ", Warn = " ", Hint = "󰰂 ", Info = " " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
-vim.diagnostic.config({ virtual_text = { prefix = '●' } })
+vim.diagnostic.config({ virtual_text = { prefix = "●" } })
 
 local dap_servers = {
   "debugpy",
-  "vscode-node-debug2"
+  "vscode-node-debug2",
 }
 
 local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_keymap(...)
+    vim.api.nvim_buf_set_keymap(bufnr, ...)
+  end
   -- local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
   require("lsp-inlayhints").on_attach(client, bufnr)
-  require('keybindings').maplsp(buf_set_keymap) -- Bind shortcut
+  require("keybindings").maplsp(buf_set_keymap) -- Bind shortcut
 end
 
 -- cmp-nvim lsp setup
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-require("mason-lspconfig").setup_handlers {
-  function (server_name)
-    require("lspconfig")[server_name].setup {
+require("mason-lspconfig").setup_handlers({
+  function(server_name)
+    require("lspconfig")[server_name].setup({
       on_attach = on_attach,
       capabilities = capabilities,
-      settings = lsp_settings(server_name)
-    }
-  end
-}
+      settings = lsp_settings(server_name),
+    })
+  end,
+})
 
 -- Load clangd lsp with extension
-require("clangd_extensions").setup {
+require("clangd_extensions").setup({
   ast = {
     role_icons = {
       type = "",
@@ -63,5 +65,4 @@ require("clangd_extensions").setup {
       TemplateParamObject = "",
     },
   },
-}
-
+})
