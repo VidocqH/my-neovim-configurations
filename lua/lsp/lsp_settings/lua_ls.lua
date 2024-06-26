262f46d1f5a41f9e3f494f9af8735e65783cb894
Vidocq <vidocqho@gmail.com>
2024-06-26 20:48:50 +0800

    lua lsp enable hint

--- lua/lsp/lsp_settings/lua_ls.lua
+++ lua/lsp/lsp_settings/lua_ls.lua
@@ -1,17 +1,17 @@
-local runtime_path = vim.split(package.path, ';')
+local runtime_path = vim.split(package.path, ";")
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 return {
   Lua = {
     runtime = {
       -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-      version = 'LuaJIT',
+      version = "LuaJIT",
       -- Setup your lua path
       path = runtime_path,
     },
     diagnostics = {
       -- Get the language server to recognize the `vim` global
-      globals = {'vim'},
+      globals = { "vim" },
     },
     workspace = {
       -- Make the server aware of Neovim runtime files
@@ -19,7 +19,10 @@ return {
     },
     -- Do not send telemetry data containing a randomized but unique identifier
     telemetry = {
-      enable = false
+      enable = false,
+    },
+    hint = {
+      enable = true,
     },
   },
 }
