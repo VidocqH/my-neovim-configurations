58de661250c4158255504ca2fd987a31cef62d58
VidocqH <vidocqho@gmail.com>
2024-07-01 19:07:36 +0800

    fix neovide configs

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -2,5 +2,5 @@
 vim.g.neovide_remember_window_size = true
 vim.g.neovide_cursor_vfx_mode = "railgun"
 -- vim.opt.guifont = { "RobotoMono Nerd Font", ":h14" }
--- vim.opt.guifont = { "Lilex Nerd Font", ":h14" }
-vim.g.neovide_input_macos_alt_is_meta = true
+vim.opt.guifont = { "Lilex Nerd Font", ":h14" }
+vim.g.neovide_input_option_alt_is_meta = true
