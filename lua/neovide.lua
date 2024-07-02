b2c22a9ff105dcbe58a513f81f689a7211188387
VidocqH <vidocqho@gmail.com>
2024-07-02 16:04:44 +0800

    adjust railgun effect density

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,6 +1,7 @@
 -- vim.g.neovide_fullscreen = true
 vim.g.neovide_remember_window_size = true
 vim.g.neovide_cursor_vfx_mode = "railgun"
+vim.g.neovide_cursor_vfx_particle_density = 14.0
 -- vim.opt.guifont = { "RobotoMono Nerd Font", ":h14" }
 vim.opt.guifont = { "Lilex Nerd Font", ":h14" }
 vim.g.neovide_input_option_alt_is_meta = true
