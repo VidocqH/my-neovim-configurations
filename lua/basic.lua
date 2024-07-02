e101a739108b9aab8abe4d08febedaade929ad2d
VidocqH <vidocqho@gmail.com>
2024-07-02 19:18:30 +0800

    hard code python provider

--- lua/basic.lua
+++ lua/basic.lua
@@ -7,6 +7,8 @@ set.shiftwidth = 2
 set.softtabstop = 0
 set.expandtab = true
 
+vim.g.python3_host_prog = "/Users/vidocq/.pyenv/versions/3.10.13/envs/nvim/bin/python3"
+
 set.cursorline = true
 -- if IS_VSCODE == false then
 --   set.cursorcolumn = true
