f33abea8e66f972a090e346423a5442717056d28
VidocqH <vidocqho@gmail.com>
2025-01-06 01:47:01 +0800

    show wakatime info in lualine

--- lua/plugin-config/lualine-config.lua
+++ lua/plugin-config/lualine-config.lua
@@ -1,4 +1,49 @@
-require('lualine').setup {
+local Job = require("plenary.job")
+local async = require("plenary.async")
+
+local get_wakatime_time = function()
+  local tx, rx = async.control.channel.oneshot()
+  local ok, job = pcall(Job.new, Job, {
+    -- command = os.getenv("HOME") .. "/.wakatime/wakatime-cli",
+    command = "wakatime-cli",
+    args = { "--today" },
+    on_exit = function(j, _)
+      tx(j:result()[1] or "")
+    end,
+  })
+  if not ok then
+    vim.notify("Bad WakaTime call: " .. job, "warn")
+    return ""
+  end
+
+  job:start()
+  return rx()
+end
+
+local WAKATIME_UPDATE_INTERVAL = 10000
+local comp_wakatime_time = ""
+
+-- Yield statusline value
+local wakatime = function()
+  if not Wakatime_routine_init then
+    local timer = vim.uv.new_timer()
+    if timer == nil then
+      return ""
+    end
+    -- Update wakatime every 10s
+    vim.uv.timer_start(timer, 500, WAKATIME_UPDATE_INTERVAL, function()
+      async.run(get_wakatime_time, function(time)
+        comp_wakatime_time = time
+      end)
+    end)
+
+    Wakatime_routine_init = true
+  end
+
+  return comp_wakatime_time
+end
+
+require("lualine").setup({
   sections = {
     lualine_a = {
       { "mode", separator = { left = "" }, right_padding = 2 },
@@ -6,7 +51,19 @@ require('lualine').setup {
     lualine_c = {
       { "filename", file_status = false, path = 1, color = { fg = "#7f848e" } },
     },
-    lualine_x = { "encoding", "filetype", "progress" },
+    lualine_x = {
+      "encoding",
+      "filetype",
+      "progress",
+      {
+        wakatime,
+        -- cond = function()
+        --   return vim.g["loaded_wakatime"] == 1
+        -- end,
+        icon = "󱑆",
+        color = { fg = "#73ddec" },
+      },
+    },
     lualine_y = { { "datetime", style = "%I:%M:%S %p" } },
     lualine_z = {
       { "location", separator = { right = "" }, left_padding = 2 },
@@ -29,4 +86,4 @@ require('lualine').setup {
     },
   },
   extensions = { "toggleterm", "aerial", "nvim-dap-ui", "neo-tree" },
-}
+})
