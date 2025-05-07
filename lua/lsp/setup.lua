32b770ac2a383224b11e3ddb90feacabb90a7bb4
VidocqH <vidocqho@gmail.com>
2025-05-07 19:22:33 +0800

    lsp config for nvim 0.11

--- lua/lsp/setup.lua
+++ lua/lsp/setup.lua
@@ -1,10 +1,15 @@
 -- vim.lsp.set_log_level("debug")
 vim.lsp.set_log_level("off")
 
+local function get_typescript_server_path(root_dir)
+  local project_root = vim.fs.dirname(vim.fs.find("node_modules/typescript/lib", { path = root_dir, upward = true })[1])
+  return project_root and (project_root .. "/lib") or ""
+end
+
 local function lsp_settings(server_name)
   if server_name == "lua_ls" then
     return require("lsp.lsp_settings.lua_ls")
-  elseif server_name == "tsserver" then
+  elseif server_name == "ts_ls" then
     return require("lsp.lsp_settings.tsserver")
   elseif server_name == "basedpyright" then
     return require("lsp.lsp_settings.basedpyright")
@@ -36,15 +41,41 @@ end
 -- cmp-nvim lsp setup
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-require("mason-lspconfig").setup_handlers({
-  function(server_name)
-    require("lspconfig")[server_name].setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
-      settings = lsp_settings(server_name),
-    })
+vim.lsp.config("*", { on_attach = on_attach, capabilities = capabilities })
+vim.lsp.config("mdx_analyzer", {
+  on_new_config = function(new_config, new_root_dir)
+    if vim.tbl_get(new_config.init_options, "typescript") and not new_config.init_options.typescript.tsdk then
+      new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
+    end
   end,
 })
+vim.lsp.config("lua_ls", { settings = require("lsp.lsp_settings.lua_ls") })
+vim.lsp.config("ts_ls", { settings = require("lsp.lsp_settings.tsserver") })
+vim.lsp.config("basedpyright", { settings = require("lsp.lsp_settings.basedpyright") })
+
+require("mason-lspconfig").setup()
+
+-- require("mason-lspconfig").setup_handlers({
+--   function(server_name)
+--     if server_name == "mdx_analyzer" then
+--       require("lspconfig").mdx_analyzer.setup({
+--         on_attach = on_attach,
+--         capabilities = capabilities,
+--         on_new_config = function(new_config, new_root_dir)
+--           if vim.tbl_get(new_config.init_options, "typescript") and not new_config.init_options.typescript.tsdk then
+--             new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
+--           end
+--         end,
+--       })
+--       return
+--     end
+--     require("lspconfig")[server_name].setup({
+--       on_attach = on_attach,
+--       capabilities = capabilities,
+--       settings = lsp_settings(server_name),
+--     })
+--   end,
+-- })
 
 -- Load clangd lsp with extension
 require("clangd_extensions").setup({
