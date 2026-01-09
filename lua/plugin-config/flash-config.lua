9ef9e13c24cd9309a94832a8832ce0e6a6f2080e
VidocqH <vidocqho@gmail.com>
2026-01-09 09:54:44 +0800

    fix flash

--- lua/plugin-config/flash-config.lua
+++ lua/plugin-config/flash-config.lua
@@ -26,5 +26,43 @@ vim.api.nvim_create_user_command("FlashLine", function()
 end, {})
 
 vim.api.nvim_create_user_command("FlashWord", function()
-  Flash.jump({ modes = { char = { jump_labels = true } } })
+  -- Flash.jump({ modes = { char = { jump_labels = true } } })
+  Flash.jump({
+    search = { mode = "search" },
+    label = { after = false, before = { 0, 0 }, uppercase = false, format = format },
+    pattern = [[\<]],
+    action = function(match, state)
+      state:hide()
+      Flash.jump({
+        search = { max_length = 0 },
+        highlight = { matches = false },
+        label = { format = format },
+        matcher = function(win)
+          -- limit matches to the current label
+          return vim.tbl_filter(function(m)
+            return m.label == match.label and m.win == win
+          end, state.results)
+        end,
+        labeler = function(matches)
+          for _, m in ipairs(matches) do
+            m.label = m.label2 -- use the second label
+          end
+        end,
+      })
+    end,
+    labeler = function(matches, state)
+      local labels = state:labels()
+      for m, match in ipairs(matches) do
+        match.label1 = labels[math.floor((m - 1) / #labels) + 1]
+        match.label2 = labels[(m - 1) % #labels + 1]
+        match.label = match.label1
+      end
+    end,
+  })
 end, {})
+-- Flash.jump({
+--   modes = { char = { jump_labels = true } },
+--   search = { mode = "search", max_length = false },
+--   pattern = "\\w\\+",
+--   label = { before = { 0, 0 }, after = false },
+-- })
